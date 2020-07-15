8feee62d1ddbd686512b073e67bdb9b10c877eff
AdamWeissman <adam@comedytragedyepic.com>
2020-07-15 17:10:20 -0400

    controlled element

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,10 @@ import SearchBar from './SearchBar';
 
 const App = () => {
   return( 
-    <div>
+    <div className="ui container" style={{ marginTop: '10px'}}>
       <SearchBar />
-    </div>)
+    </div>
+    )
 };
 
 export default App;
