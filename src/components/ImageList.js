09f44ebed02dbef33febd8276c9b9daeda0593e3
AdamWeissman <adam@comedytragedyepic.com>
2020-07-16 17:37:32 -0400

    app finished

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,15 @@
+import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard'
 
 
 //if you wanted the list item to be in a div, you'd need to put the key on the div
-
+// also note how destructuring converts the iterator in place. const images = props.images.map(({ description, id, urls }) => {
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular} />
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} pic={image} />
   })
-  return <div>{images}</div>
+  return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
