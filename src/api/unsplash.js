ba14990fc52e75889242c13dd031af3cbac826ec
AdamWeissman <adam@comedytragedyepic.com>
2020-07-16 16:19:01 -0400

    gitignore try again

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import React from 'react';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-  huh? why doesn't this get ignored?
-  }
-});
