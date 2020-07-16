d38106b786b2dd654a258ba31e44a093eed57660
AdamWeissman <adam@comedytragedyepic.com>
2020-07-16 16:08:28 -0400

    trying git ignore now that file is moved

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
         params: { query: term },
-        headers: {
-          Authorization: 'Client-ID '
-        }
+        
       });
       // }).then((responseFromUnsplash) => {
       //   console.log(responseFromUnsplash.data.results)
       // }); <- this promise based syntax, an alternate to async await syntax
-    console.log(response.data.results);
+    this.setState ({ images: response.data.results });
   }
 
   render() { 
   return ( 
     <div className="ui container" style={{ marginTop: '10px'}}>
       <SearchBar myOnSubmit={this.onSearchSubmit} />
+      Found: {this.state.images.length} images
     </div>
     );
   }
