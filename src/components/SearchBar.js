4de55946e2b17f5a49150aa291a17101a652ab64
AdamWeissman <adam@comedytragedyepic.com>
2020-07-16 14:11:15 -0400

    ran npm install --ssave axios

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  
   state = { term: ''}
 
-  onFormSubmit(event) {
+  onFormSubmit = (event) => {
     event.preventDefault();
-
-    console.log(this.state.term)
+    this.props.myOnSubmit(this.state.term);
   }
 
 
