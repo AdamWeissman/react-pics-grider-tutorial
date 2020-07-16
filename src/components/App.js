9980fe552c645df3a80bfdbcf20d19c8dd988501
AdamWeissman <adam@comedytragedyepic.com>
2020-07-16 15:49:31 -0400

    promise and async tested, took out authentication key as not yet added to gitignore

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,17 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID '
+        }
+      });
+      // }).then((responseFromUnsplash) => {
+      //   console.log(responseFromUnsplash.data.results)
+      // }); <- this promise based syntax, an alternate to async await syntax
+    console.log(response.data.results);
   }
 
   render() { 
